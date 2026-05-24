db6206a7a0d9bb38d3253b5de11e41b0d08f67c7
Paul <paulo.guetan@gmail.com>
2026-05-24 11:13:28 +0800

    Revert to static Vite site

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  root: 'html',
   build: {
-    outDir: 'dist',
+    outDir: '../dist',
     emptyOutDir: true,
   },
   server: {
